cd8febd8ffb07d6f7874701c981c26470abb6772
quojus <60073916+quojus@users.noreply.github.com>
2024-01-11 16:48:17 +0100

    Update hexagonal_plot.py
    plot_hexagonal_axial

--- hexagonal_plot.py
+++ hexagonal_plot.py
@@ -69,3 +69,58 @@ def plot_hexagonal_grid(data, grid_size=(20, 20), colormap='viridis', title=None
     plt.colorbar(plt.cm.ScalarMappable(cmap=colormap, norm=plt.Normalize(vmin=vmin, vmax=vmax)), ax=ax)
     plt.show()
 
+def plot_hexagonal_axial(data, colormap='viridis', title=None, show_axes=False, label_fontsize=8, label_weight='bold', labels=None):
+    """
+    Plottet ein sechseckiges Gitter basierend auf Axialkoordinaten mit optionalen Labels.
+
+    :param data: Dictionary, das Hexagon-Koordinaten (Schlüssel) und zugeordnete Werte (Werte) enthält. Jeder Schlüssel ist ein Tupel (q, r).
+    :param colormap: Farbschema für die Plots.
+    :param title: Titel für das Diagramm.
+    :param show_axes: Bestimmt, ob die Achsen angezeigt werden sollen.
+    :param label_fontsize: Schriftgröße für die Labels.
+    :param label_weight: Schriftstärke für die Labels.
+    :param labels: Optional. Dictionary, das Hexagon-Koordinaten (als Schlüssel) und Textlabels (als Werte) enthält.
+    """
+    if not isinstance(data, dict):
+        raise ValueError("Parameter 'data' muss ein Dictionary sein.")
+    if labels is not None and not isinstance(labels, dict):
+        raise ValueError("Parameter 'labels' muss ein Dictionary sein.")
+
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+
+    if not show_axes:
+        ax.axis('off')
+
+    # Der Radius eines Sechsecks
+    radius = 1.0
+
+    vmin = min(data.values())
+    vmax = max(data.values())
+    colorType_normal:bool = True
+    if vmax == vmin:
+        colorType_normal = False
+
+
+    for (q, r), value in data.items():
+        x = q * 1.5 * radius
+        y = np.sqrt(3) * (r + q/2) * radius
+        if colorType_normal:
+            fc = plt.cm.get_cmap(colormap)((value-vmin)/(vmax-vmin))
+        else:
+            fc = plt.cm.get_cmap(colormap)(1)
+        hexagon = mpatches.RegularPolygon((x, y), numVertices=6, radius=radius, orientation=np.radians(30),
+                                          facecolor=fc, edgecolor='k')
+        ax.add_patch(hexagon)
+
+        if labels and (q, r) in labels:
+            ax.text(x, y, labels[(q, r)], ha='center', va='center', fontsize=label_fontsize, fontweight=label_weight)
+
+    if title:
+        plt.title(title)
+
+    ax.autoscale_view()
+    plt.colorbar(plt.cm.ScalarMappable(cmap=colormap, norm=plt.Normalize(vmin=vmin, vmax=vmax)), ax=ax)
+    plt.show()
+
+
